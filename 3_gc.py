748c34d6fb22c18acef3261616ac242a336c5ab9
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2023-04-13 23:02:24 +1000

    done

--- 3_gc.py
+++ 3_gc.py
@@ -1,10 +1,18 @@
 import os
 import pandas as pd
 
-def dms_to_decimal(dms):
+def dms_to_decimal_lat(dms):
     degrees, minutes, seconds = [float(i) for i in dms.split(':')]
     decimal = degrees + (minutes / 60.0) + (seconds / 3600.0)
     return decimal
+
+def dms_to_decimal_lon(dms):
+    degrees, minutes, seconds = [float(i) for i in dms.split(':')]
+    decimal = degrees + (minutes / 60.0) + (seconds / 3600.0)
+    if decimal > 180:
+        decimal = decimal - 360
+    return decimal
+
 
 rr_file = "data/meanrr.csv"
 station_file = "data/stations.csv"
@@ -17,8 +25,8 @@ rr_df = rr_df.merge(station_df, how="inner", left_on="stid", right_on="STAID")
 rr_df = rr_df.drop(columns=["STAID"], axis=1)
 rr_df.columns = rr_df.columns.str.strip()
 for i in range(len(rr_df)):
-    rr_df.at[i,'LAT'] = dms_to_decimal(rr_df.iloc[i]['LAT'])
-    rr_df.at[i,'LON'] = dms_to_decimal(rr_df.iloc[i]['LON'])
+    rr_df.at[i,'LAT'] = dms_to_decimal_lat(rr_df.iloc[i]['LAT'])
+    rr_df.at[i,'LON'] = dms_to_decimal_lon(rr_df.iloc[i]['LON'])
 rr_df = rr_df[["stid","rr","LON","LAT"]]
 rr_df = rr_df.set_axis(["stid","rr","lon","lat"], axis=1)
 rr_df.to_csv("data/rrs.csv",index=False)
--- 4_sort.py
+++ 4_sort.py
@@ -2,10 +2,10 @@ import os
 import pandas as pd
 
 vis = pd.read_csv("data/vis_with_empty.csv")
-vis = vis.sort_values(['lon', 'lat'],ascending = [False, True])
+vis = vis.sort_values(['lon', 'lat'],ascending = [True, True])
 vis.to_csv("data/vis_sorted.csv", index=False)
 
 vis = pd.read_csv("data/rrs.csv")
-vis = vis.sort_values(['lon', 'lat'],ascending = [False, True])
+vis = vis.sort_values(['lon', 'lat'],ascending = [True, True])
 vis.to_csv("data/rrs_sorted.csv", index=False)
 
